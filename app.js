3c25dfac9b91b5e3b18344a7c9ec80c362b5fe7a
Max Base <maxbasecode@gmail.com>
2022-09-16 15:59:00 +0200

    clean code app.js

--- app.js
+++ app.js
@@ -1,40 +1,47 @@
-let menu = document.getElementById("menu");
-let dificultadesUl = document.getElementById("dificultades");
-let dificultades = document.getElementsByClassName("dificultad");
-let menuPersonalizado = document.getElementById("menu-personalizado");
-let buscaminas = document.getElementById("buscaminas");
-let jugarBtn = document.getElementById("jugar");
-let backBtn = document.getElementById("back-btn");
+// Elements
+const menu = document.getElementById("menu");
+const dificultadesUl = document.getElementById("dificultades");
+const dificultades = document.getElementsByClassName("dificultad");
+const menuPersonalizado = document.getElementById("menu-personalizado");
+const buscaminas = document.getElementById("buscaminas");
+const jugarBtn = document.getElementById("jugar");
+const backBtn = document.getElementById("back-btn");
+
+// Variables
 let tablero;
 
-// resetear la clase de los botones de dificultades
+// Functions
+//// resetear la clase de los botones de dificultades
 function resetDificultades() {
     for (const i of dificultades) {
         i.className = "dificultad";
     }
 }
 
-// agregar los listener de los botones de dificultades
-for (const dificultad of dificultades) {
+// Events
+//// agregar los listener de los botones de dificultades
+dificultades.forEach((dificultad) => {
     dificultad.addEventListener("click", (e) => {
         resetDificultades();
+
         e.target.className += " seleccionado";
+
         menuPersonalizado.style.transform = "translate(2000px, 0)";
         menuPersonalizado.style.visibility = 'hidden';
         menuPersonalizado.style.opacity = '0';
     });
-}
-
-// agregar el listener el boton de dificultad personalizada
+});
+//// agregar el listener el boton de dificultad personalizada
 dificultades[dificultades.length - 1].addEventListener("click", (e) => {
     resetDificultades();
+
     e.target.className += " seleccionado";
+
     menuPersonalizado.style.transform = "translate(0, 0)";
     menuPersonalizado.style.visibility = 'visible';
     menuPersonalizado.style.opacity = '1';
 });
-
-// agregar el listener el boton de jugar
+//// agregar el listener el boton de jugar
 jugarBtn.addEventListener("click", (e) => {
     let seleccionado = document.getElementsByClassName("seleccionado")[0];
     let ancho;
@@ -63,16 +70,20 @@ jugarBtn.addEventListener("click", (e) => {
         minas = parseInt(minasEntry.value);
 
 
-        if (!(5 < alto) || !(alto <= 40) || !(5 < ancho) || !(ancho <= 40) || !(1 < minas) || !(minas < ancho * alto)) {
+        if (!(5 < alto) ||
+            !(alto <= 40) ||
+            !(5 < ancho) ||
+            !(ancho <= 40) ||
+            !(1 < minas) ||
+            !(minas < ancho * alto)
+        ) {
             if (!(1 < minas) || !(minas < ancho * alto)) {
                 minasEntry.style.borderColor = "red";
             } else {
                 minasEntry.style.borderColor = "black";
             }
             return false;
-        } 
-        
-
+        }
     }
 
     menu.style.display = "none";
@@ -81,8 +92,7 @@ jugarBtn.addEventListener("click", (e) => {
 
     tablero = new Buscaminas(ancho, alto, minas);
 });
-
-
+//// black button
 backBtn.addEventListener("click", (e) => {
     menu.style.display = "flex";
     buscaminas.style.display = "none";
